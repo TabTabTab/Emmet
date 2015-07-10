0dad7faf93501135f000193a93fd6bc1bc2bd807
Johan Nyholm <hansjohannyholm@gmail.com>
2015-07-10 21:23:29 +0200

    Add simple circle

--- src/shapes.py
+++ src/shapes.py
@@ -68,3 +68,60 @@ class Square(Figure):
                 pass
         return grid
 
+class Circle(Figure):
+    def __init__(self, pos_x=0, pos_y=0, radius=3):
+        super().__init__(pos_x, pos_y)
+        self.radius = radius
+        self.dot = '+'
+
+    def grow(self, rad_inc = 1):
+        self.radius += rad_inc
+
+    def _update_sklt(self):
+        self.sklt = [Point(self.x_pos,
+                           self.y_pos),
+                     Point(self.x_pos + self.radius * 2,
+                           self.y_pos + self.radius * 2)]
+
+    def _is_inside(self, x, y, rad, center):
+        rad2 = rad * rad
+        dist2 = (x - center[0])**2 + (y-center[1])**2
+        return dist2 <= rad2
+
+    def draw(self, grid):
+        # ugly temporary way of drawing a circle
+        self._update_sklt()
+        (width, height) = self.grid_size(grid)
+        #left upper corner
+        luc = self.point_to_pix(self.sklt[0], width, height)
+        #right lower corner
+        rlc = self.point_to_pix(self.sklt[1], width, height)
+
+        rad_x = (rlc[0] - luc[0]) / 2
+        rad_y = (rlc[1] - luc[1]) / 2
+
+        rad = 0
+        y_fact = 1
+        x_fact = 1
+        if rad_x < rad_y:
+            x_fact = rad_y / rad_x
+            rad = rad_y
+        else:
+            y_fact = rad_x / rad_y
+            rad = rad_x
+        cx = luc[0] + rad_x
+        cy = luc[1] + rad_y
+        center = (cx, cy)
+
+        for y in range(luc[1], rlc[1] + 1):
+            try:
+                for x in range(luc[0], rlc[0] + 1):
+                    x_diff = (cx - x) * x_fact
+                    y_diff = (cy - y) * y_fact
+                    X = cx + x_diff
+                    Y = cy + y_diff
+                    if(self._is_inside(X, Y, rad, center)):
+                        grid[y][x] = self.dot
+            except IndexError:
+                pass
+        return grid
